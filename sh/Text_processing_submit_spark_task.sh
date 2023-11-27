18f06ac4619c2111b4e2adfc256be69dafc9c0d6
anyuecq25 <chenq@swu.edu.cn>
2023-11-27 11:22:32 +0800

    Submit Spark task for Algorithm 1 and 2 without tackling 20000 paragraphs problem.

--- sh/Text_processing_submit_spark_task.sh
+++ sh/Text_processing_submit_spark_task.sh
@@ -1,4 +1,5 @@
-#submit spark task for Algorithm 1 and 2 without tackling 20000 paragraphs problem.
+#Submit Spark task for Algorithm 1 and 2 without tackling 20000 paragraphs problem.
+#Results shown in Table 3 as '1 nodes' etc.
 export PYSPARK_DRIVER_PYTHON=/home/ubuntu/env3.5/bin/Python3.5
 PYSPARK_PYTHON=./VENV/env3.5/bin/Python3
 Spark-submit --conf Spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/env3.5 /bin/Python3 \
