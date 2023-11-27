bfa186bbb3f2c3b31ef317a9cb0b0a43c5c32230
anyuecq25 <chenq@swu.edu.cn>
2023-11-27 11:07:40 +0800

    Algorithm 1, 2 without tackling 20000 paragraphs

---
+++ rdd_read_docx.py
@@ -0,0 +1,175 @@
+#Text processing as depicted in Alogirhtm1,2 except tackling 20000 paragraph problem. Nov.25 2023
+from pyspark.sql import SparkSession
+
+#from __future__ import print_function
+
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+import os
+
+#PYSPARK_PYTHON = "/home/ubuntu/env3.5/bin/python3.5"
+#os.environ["PYSPARK_PYTHON"] = PYSPARK_PYTHON
+
+
+
+from pyspark.ml.feature import HashingTF,IDF,Tokenizer
+
+import sys
+import time
+def 𝑝𝑟𝑜𝑐𝑒𝑠𝑠𝑖𝑛𝑔_𝑓𝑖𝑙𝑒𝑠_𝑡𝑥𝑡(input_docx_path):
+	start = time.clock()
+
+
+
+	docs = docx.Document(input_docx_path)
+	text_list=[]
+	n_p =len(docs.paragraphs)
+	# if (n_p > 20000):
+	# 	res = 'paragraph count >20000'
+	# 	print(input_docx_path+' paragraph count >2000  is: '+str(n_p))
+	# 	return res
+	for i in range(n_p):
+		string_=docs.paragraphs[i].text
+		#if string_ not in text_list and string_!=' ' and string_!=' ' and len(string_)>0:   #not in too slow
+		if string_ != ' ' and string_ != ' ' and len(string_) > 0:
+			text_list.append(string_.strip())
+
+	#ith =0
+	for table in docs.tables:
+	    for row in table.rows:
+	        for cell in row.cells:
+	            text_list.append(cell.text.strip())   # new method faster than before 190s->24s by qchen 2021.2.7
+	            #ith=ith + 1
+	            #if ith%50==0:
+	                #print(ith)
+
+	# for i in range(len(docs.tables)):
+	# 	for j in range(len(docs.tables[i]._cells)):
+	# 		string_=docs.tables[i]._cells[j].text
+	# 		#if string_ not in text_list and string_!=' ' and string_!=' ' and len(string_)>0:
+	# 		if string_ != ' ' and string_ != ' ' and len(string_) > 0:
+	# 			text_list.append(string_.strip())
+
+
+
+    #summary = tables[0].rows[6].cells[1].text
+    #print(text_list)
+	duration=int((time.clock() - start))
+	if duration>1:
+		print(input_docx_path+' , execution time:\t' + str(duration) + 's')
+
+	return " ".join(text_list)
+
+#spark_conf = SparkConf().setAppName('rdd_read_docx_6_nodes_store_tmp_20cores_16G_tackle_word_cannot_read_problem')
+spark_conf = SparkConf()#.setAppName('rdd_read_docx_func_call_testing_115_docs')
+#spark_conf = SparkConf().setAppName('rdd_read_docx_no_read_docx')
+#spark_conf = SparkConf().setAppName('rdd_read_docx_v4_20cores_7nodes')
+
+    # .setMaster(master)
+
+sc = SparkContext(conf=spark_conf)
+#sc.setLogLevel('ERROR')
+spark = SparkSession.builder.appName("PythonKMeans").getOrCreate()
+
+total=sc.accumulator(0)
+
+import os
+import docx
+import shutil
+def main_procedure(x,filename):
+	#return ""
+	filepath, fullflname = os.path.split(filename)
+	docfile='/dev/shm/'+fullflname
+	docfile = '/tmp/' + fullflname
+	res=""
+	#print('in func.')
+	try:
+		with open(docfile, 'wb') as f:
+			f.write(x)
+			f.close()
+			#docs = docx.Document('/tmp/'+filename)
+			try:
+				res=𝑝𝑟𝑜𝑐𝑒𝑠𝑠𝑖𝑛𝑔_𝑓𝑖𝑙𝑒𝑠_𝑡𝑥𝑡(docfile)
+				#print('call read func once in shm 200 record time.')
+				#print('1')
+				#with open(docfile+'_tmp','w') as t:
+				#	t.write("")
+				#	t.close()
+			except Exception as e:
+				print('exception: '+str(e))
+	except Exception:
+		a=3
+
+	#import shutil
+	tmp_path=docfile
+	os.remove(docfile)
+	#print(res)
+	print(fullflname+' *optimized table reading in tmp * file length :' + str(len(res)))
+	return res
+	#for i in os.listdir(tmp_path):
+	#	if os.path.isdir(os.path.join(tmp_path, i)):
+	#		shutil.rmtree(os.path.join(tmp_path, i))
+
+
+path='file:///home/ubuntu/chenq/docx_evaluate_score/data/all_docx/input/*.docx'
+path='file:///home/ubuntu/chenq/test_docx/input/*.docx'
+path='file:////dev/shm/input/*.docx'
+path='file:////dev/shm/input2/input/*.docx'
+path='/user/ubuntu/docx/input/*.docx'
+path='/user/ubuntu/test_docx/input/*.docx'  #20201119   ncount=10345
+path='/user/ubuntu/chenq/all_docx/input/*.docx'  #20201119   ncount=175420
+#path='./chenq/mini_test_docx/input/*.docx'  #desktop5 mini_test file count:115 on hdfs
+#path='har:////user/ubuntu/ainput/docx.har/input/*.docx'
+rdd = sc.binaryFiles(path)
+
+#rdd.cache()
+#rdd.count()
+#rdd=rdd.repartition(32)
+
+# df=rdd.toDF()
+#
+# from pyspark.sql.functions import *
+# from pyspark.sql.types import *
+# from pyspark.sql.types import Row
+#
+# readUDF=udf(lambda a,z:read(a,z),StringType())
+#
+# df_new=df.select('_1',readUDF('_2','_1').alias('content'))
+
+doc=rdd.map(lambda x:(x[0],main_procedure(x[1],x[0])))
+
+#doc.foreach(print)
+doc.cache()
+nread=doc.count()
+#df_new.cache()
+#nread=df_new.count()
+print(nread)
+
+#df_new.rdd.saveAsTextFile('./chenq/df_rdd1.csv')
+df=doc.toDF()
+#df_new.write.parquet('./chenq/4_4_16_df_114.parquet')
+#df_new.write.parquet('./chenq/4_4_16_df_175420.parquet')
+#df.write.parquet('./chenq/all_docx_175420_v23_6_nodes_tmp_20cores_16G_tackle_word_cannot_read_problem.parquet')
+#df.write.parquet('./chenq/trash_115.parquet')
+#df.write.parquet('./chenq/test_docx_1.parquet')
+
+sc.stop()
+'''
+while(nread>0):
+	rdd1 = sc.binaryFiles(path)
+	doc1 = rdd1.map(lambda x: (x[0], read(x[1], x[0])))
+	nread = doc1.count()
+	if(nread>0):
+		doc=doc.union(doc1)
+		print('This time read: '+str(nread))
+
+print('total count is:'+str(doc.count()))
+#print(doc.first()[0])
+#print(doc.first()[1])
+'''
+
+
+
+
+
