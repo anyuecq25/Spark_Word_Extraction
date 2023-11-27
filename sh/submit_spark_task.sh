d0a88051cce5236eaa02673851c8c042cd9e50cf
anyuecq25 <chenq@swu.edu.cn>
2023-11-27 10:52:49 +0800

    Add files via upload
    Submit task for text processing. Algorithm 1 .

---
+++ sh/submit_spark_task.sh
@@ -0,0 +1,9 @@
+export PYSPARK_DRIVER_PYTHON=/home/ubuntu/env3.5/bin/Python3.5
+PYSPARK_PYTHON=./VENV/env3.5/bin/Python3
+Spark-submit --conf Spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/env3.5 /bin/Python3 \
+--master yarn-client \
+--executor-memory 32g \
+--driver-memory 10g \
+--executor-cores 16 \
+--num-executors 4 \
+--archives env35.zip#VENV rdd_read_docx.py
