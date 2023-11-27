0cbc96a36f1b2ec4b8b21a8778587f7cf30176a0
anyuecq25 <chenq@swu.edu.cn>
2023-11-27 10:53:57 +0800

    Delete exec_core_combines_.sh

--- exec_core_combines_.sh
+++
@@ -1,7 +0,0 @@
-sh submit_6_16cores_16G_do_not_tackle_200_paragraphs.sh
-sh exe_core_combine_32G_4executors_16cores_yarn.sh
-sh exe_core_combine_16G_8executors_8cores_yarn.sh
-sh exe_core_combine_8G_16executors_4cores_yarn.sh
-sh exe_core_combine_4G_32executors_2cores_yarn.sh
-sh submit_2G_4cores_16executors_yarn.sh 
-ls
